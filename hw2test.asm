ba2217e079e629752a704f6a30c409f915a687ea
woundeddophin <justfrznh2o@gmail.com>
2015-02-01 13:33:39 -0800

    Improve asm files
    shortened getpid.asm
    added test file hw2test.asm

--- hw2test.asm
+++ hw2test.asm
@@ -6,9 +6,18 @@ SET  r0 2   #r0 = SYSCALL_GETPID
 PUSH r0
 TRAP
 
+#store the PID in r1
+POP  r1
+
 #output system call
 SET  r0 1    #r0 = SYSCALL_OUTPUT
+PUSH r1
 PUSH r0
+TRAP
+
+#core dump system call
+SET R0 9
+PUSH R0
 TRAP
 
 #Though puzzle:  How could you make this 
